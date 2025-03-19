951dfded1b2aee43aeda4ddcb542750dc8ddb154
ajinkya082 <ajinkyaraut0804@gmail.com>
2025-03-19 18:44:32 +0530

    Add files via upload

---
+++ Day 3/stnt.java
@@ -0,0 +1,26 @@
+public class stnt{
+    public static void main(String[] args) {
+        boolean b=false;;
+        if(false==false);       
+        if(false==false)
+            System.out.println("ankita");
+        else
+            System.out.println("ji");
+        int v=10;
+        if(v++==10 & true){
+            System.out.println("hi");
+        }
+        else{
+            System.out.println("hello");
+        }
+        int d=10,y=20,z=30;
+        if(true&true){
+            System.out.println(d+y+"hello"+z+z);
+            System.out.println('a'+y+"hello"+z);
+        }else{
+            System.out.println("hi");
+        }
+
+        
+    }
+}
